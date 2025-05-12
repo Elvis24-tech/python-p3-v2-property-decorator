98428852c49c8b45dea71e1e26ab6d74637e5d9d
elvis <kariuki.elvis@student.moringaschool.com>
2025-05-12 14:56:18 +0300

    decorator

--- lib/dog.py
+++ lib/dog.py
@@ -12,28 +12,34 @@ APPROVED_BREEDS = [
 
 class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
-        self.name = name
-        self.breed = breed
+        """Initialize a Dog with name and breed."""
+        self._name = None  
+        self._breed = None  
+        self.name = name  
+        self.breed = breed  
 
-    def get_name(self):
+    @property
+    def name(self):
+        """Get the dog's name."""
         return self._name
 
-    def set_name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+    @name.setter
+    def name(self, value):
+        """Set the dog's name, ensuring it's a string between 1 and 25 characters."""
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    @property
+    def breed(self):
+        """Get the dog's breed."""
         return self._breed
 
-    def set_breed(self, breed):
-        if breed in APPROVED_BREEDS:
-            self._breed = breed
+    @breed.setter
+    def breed(self, value):
+        """Set the dog's breed, ensuring it's in the approved list."""
+        if value in APPROVED_BREEDS:
+            self._breed = value
         else:
             raise ValueError("Breed must be in list of approved breeds.")
-
-    breed = property(get_breed, set_breed)
